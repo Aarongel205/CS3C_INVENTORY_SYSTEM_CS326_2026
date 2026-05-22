3711583f3d70011223891455539478fce88c9b87
Aarongel205 <aaronadanza92@gmail.com>
2026-05-22 18:17:23 +0800

    update files

--- routes/logs.js
+++ routes/logs.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { createClient } = require('@supabase/supabase-js');
 
 const { createClient } = require('@supabase/supabase-js');
 const ws = require('ws');
